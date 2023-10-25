5bc40ccb496416d9710f6a7b3d1619f530adac8c
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2023-10-24 20:52:33 -0700

    testing first Endpoint - HTTPGET

---
+++ Airbnb/Controllers/AirbnbAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Airbnb_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airbnb_API.Controllers
+{
+    [Route("api/AirbnbAPI")]
+    [ApiController]
+    public class AirbnbAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Airbnb> GetAirbnbs()
+        {
+            return new List<Airbnb>
+            {
+                new Airbnb {Id=1, Name="Pool View"},
+                new Airbnb {Id=2, Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ Airbnb/Models/Airbnb.cs
@@ -0,0 +1,8 @@
+﻿namespace Airbnb_API.Models
+{
+    public class Airbnb
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
