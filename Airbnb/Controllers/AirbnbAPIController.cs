c4040d2ee65e8cb8ae3e81acf767a2968a00c2c2
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2023-11-13 22:12:41 -0800

    testing http delete

--- Airbnb/Controllers/AirbnbAPIController.cs
+++ Airbnb/Controllers/AirbnbAPIController.cs
@@ -2,6 +2,7 @@
 using Airbnb_API.Models;
 using Airbnb_API.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Airbnb_API.Controllers
 {
@@ -15,22 +16,22 @@ namespace Airbnb_API.Controllers
         {
             return Ok(AirbnbStore.airbnbList);
         }
-        [HttpGet("{id:int}", Name = "GetAirbnbs")]
+        [HttpGet("{id:int}", Name = "GetAirbnb")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<AirbnbDTO> GetAirbnbs(int id)
+        public ActionResult<AirbnbDTO> GetAirbnb(int id)
         {
             if(id == 0)
             {
                 return BadRequest();
             }
-            var Airbnb = AirbnbStore.airbnbList.FirstOrDefault(u => u.Id == id);
-            if (Airbnb == null)
+            var airbnb = AirbnbStore.airbnbList.FirstOrDefault(u => u.Id == id);
+            if (airbnb == null)
             {
                 return NotFound();
             }
-            return Ok(Airbnb);
+            return Ok(airbnb);
         }
 
         [HttpPost]
@@ -59,7 +60,26 @@ namespace Airbnb_API.Controllers
             airbnbDTO.Id = AirbnbStore.airbnbList.OrderByDescending(u=>u.Id).FirstOrDefault().Id+1;
             AirbnbStore.airbnbList.Add(airbnbDTO);
 
-            return CreatedAtRoute("GetAirbnbs", new { id = airbnbDTO.Id }, airbnbDTO);
+            return CreatedAtRoute("GetAirbnb", new { id = airbnbDTO.Id }, airbnbDTO);
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}", Name = "DeleteAirbnb")]
+        public IActionResult DeleteAirbnb(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var airbnb = AirbnbStore.airbnbList.FirstOrDefault(u => u.Id == id);
+            if (airbnb == null)
+            {
+                return NotFound();
+            }
+            AirbnbStore.airbnbList.Remove(airbnb);
+            return NoContent();
         }
     }
 }
