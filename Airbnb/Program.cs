ed8516fac7323633d46cf3c7caa283083441e098
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2023-12-11 23:48:55 -0800

    changing Logger with Dependency injection - Serilog

--- Airbnb/Program.cs
+++ Airbnb/Program.cs
@@ -1,6 +1,13 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+//    .WriteTo.File("log/airbnbLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+//builder.Host.UseSerilog();
 
 builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
@@ -25,3 +32,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
